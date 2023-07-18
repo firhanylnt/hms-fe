d656b48dccff1d884d916719e592f3cd912d17fb
Firhan Yulianto <firhan@visinemapictures.com>
2023-07-18 20:31:34 +0700

    update report

--- pages/report/index.vue
+++ pages/report/index.vue
@@ -14,46 +14,126 @@ export default {
             splineAreaChart : {
                 series: [
                     {
-                    name: "series1",
-                    data: [34, 40, 28, 52, 42, 109, 100]
+                    name: "Net Profit",
+                    data: [46, 57, 59, 54, 62, 58, 64, 60, 66]
                     },
                     {
-                    name: "series2",
-                    data: [32, 60, 34, 46, 34, 52, 41]
+                    name: "Revenue",
+                    data: [74, 83, 102, 97, 86, 106, 93, 114, 94]
+                    },
+                    {
+                    name: "Free Cash Flow",
+                    data: [37, 42, 38, 26, 47, 50, 54, 55, 43]
                     }
                 ],
                 chartOptions: {
+                    chart: {
+                    toolbar: {
+                        show: false
+                    }
+                    },
+                    plotOptions: {
+                    bar: {
+                        dataLabels: {
+                        position: "top" // top, center, bottom
+                        }
+                    }
+                    },
                     dataLabels: {
-                        enabled: false
+                    enabled: true,
+                    formatter: function(val) {
+                        return val + "%";
                     },
-                    stroke: {
-                        curve: "smooth",
-                        width: 3
+                    offsetY: -20,
+                    style: {
+                        fontSize: "12px",
+                        colors: ["#000000"]
+                    }
                     },
-                    colors: ['#5b73e8', '#F31559'],
-                    xaxis: {
-                    type: "datetime",
-                    categories: [
-                        "2018-09-19T00:00:00",
-                        "2018-09-19T01:30:00",
-                        "2018-09-19T02:30:00",
-                        "2018-09-19T03:30:00",
-                        "2018-09-19T04:30:00",
-                        "2018-09-19T05:30:00",
-                        "2018-09-19T06:30:00"
-                    ]
-                    },
+                    colors: ['#F31559', '#4E4FEB', '#6527BE'],
                     grid: {
                     borderColor: "#f1f1f1"
                     },
+                    xaxis: {
+                    categories: [
+                        "Jan",
+                        "Feb",
+                        "Mar",
+                        "Apr",
+                        "May",
+                        "Jun",
+                        "Jul",
+                        "Aug",
+                        "Sep",
+                        "Oct",
+                        "Nov",
+                        "Dec"
+                    ],
+                    position: "top",
+                    labels: {
+                        offsetY: -18
+                    },
+                    axisBorder: {
+                        show: false
+                    },
+                    axisTicks: {
+                        show: false
+                    },
+                    crosshairs: {
+                        fill: {
+                        type: "gradient",
+                        gradient: {
+                            colorFrom: "#D8E3F0",
+                            colorTo: "#BED1E6",
+                            stops: [0, 100],
+                            opacityFrom: 0.4,
+                            opacityTo: 0.5
+                        }
+                        }
+                    },
                     tooltip: {
-                    x: {
-                        format: "dd/MM/yy HH:mm"
+                        enabled: true,
+                        offsetY: -35
+                    }
+                    },
+                    fill: {
+                    gradient: {
+                        shade: "light",
+                        type: "horizontal",
+                        shadeIntensity: 0.25,
+                        gradientToColors: undefined,
+                        inverseColors: true,
+                        opacityFrom: 1,
+                        opacityTo: 1,
+                        stops: [50, 0, 100, 100]
+                    }
+                    },
+                    yaxis: {
+                    axisBorder: {
+                        show: false
+                    },
+                    axisTicks: {
+                        show: false
+                    },
+                    labels: {
+                        show: false,
+                        formatter: function(val) {
+                        return val + "%";
+                        }
+                    }
+                    },
+                    title: {
+                    text: "Report 2023",
+                    floating: true,
+                    offsetY: 320,
+                    align: "center",
+                    style: {
+                        color: "#000000"
                     }
                     }
                 }
-            },
-        };
+            }
+        }
     },
     middleware: "authentication",
 };
@@ -68,9 +148,9 @@ export default {
                     <div class="col">
                         <div class="card">
                             <div class="card-body">
-                                <h4 class="card-title mb-4">Spline Area</h4>
+                                <h4 class="card-title mb-4">Report</h4>
                                 <!-- Spline Area chart -->
-                                <apexchart class="apex-charts" height="350" type="area" dir="ltr" :series="splineAreaChart.series" :options="splineAreaChart.chartOptions"></apexchart>
+                                <apexchart class="apex-charts" height="300" type="bar" dir="ltr" :series="splineAreaChart.series" :options="splineAreaChart.chartOptions"></apexchart>
                             </div>
                         </div>
                     </div>
