385cd9a1fe450614424385cc0f6ce96063b7f65b
Firhan Yulianto <firhan@visinemapictures.com>
2023-07-23 22:09:41 +0700

    update

--- components/Sidebar/DoctorSidebar.vue
+++ components/Sidebar/DoctorSidebar.vue
@@ -15,5 +15,19 @@
         <span>Appointment</span>
       </nuxt-link>
     </li>
+    <li class="li-menus" id="menu-ipd">
+      <nuxt-link to="/ipd" class="side-nav-link-ref">
+        <!-- <i class="fas fa-home"></i> -->
+        <i class="uil uil-medical-drip"></i>
+        <span>IPD</span>
+      </nuxt-link>
+    </li>
+    <li class="li-menus" id="menu-opd">
+      <nuxt-link to="/opd" class="side-nav-link-ref">
+        <!-- <i class="fas fa-home"></i> -->
+        <i class="uil uil-file-medical-alt"></i>
+        <span>OPD</span>
+      </nuxt-link>
+    </li>
   </ul>
 </template>
